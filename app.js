50cdee85ef2689d076a1c56a2d15677198671b33
Dan Crews <crewsd@gmail.com>
2017-09-16 14:48:06 -0600

    Queries.graphql

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 const Schema = require('./GraphQL');
 
